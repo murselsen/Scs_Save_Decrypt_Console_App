6c4c82b27762dad6b8e73fa1d890681f2a3849b3
Mürsel Şen <46069077+murselsen@users.noreply.github.com>
2025-01-03 22:33:52 +0300

    Add initial implementation of Scs Save Decrypt Console App with requirements and documentation

---
+++ README.md
@@ -0,0 +1,38 @@
+# Scs_Save_Decrypt_Console_App
+
+# Scs Save Decrypt Console App, It is a console application developed by Scs Software that allows you to decipher files with the .sii extension. This application allows you to decipher files containing profile details of the games Euro Truck Simulator 2 and American Truck Simulator.
+
+## Features
+
+- Decrypts files with .sii extension
+- Compatible with Euro Truck Simulator 2 and American Truck Simulator
+- User-friendly command line interface
+
+## Installation
+
+1. Clone this repository:
+    ```sh
+    git clone https://github.com/username/Scs_Save_Decrypt_Console_App.git
+    ```
+2. Install the necessary dependencies:
+    ```sh
+    pip install -r requirements.txt
+    ```
+
+## Usage
+
+1. Run the application:
+    ```sh
+    python app.py --gameName --profileName --saveDisplayName
+    ```
+2. The decrypted file will be created in the same directory.
+
+## Contribution
+
+To contribute, please submit a pull request or open an issue.
+
+## License
+
+This project is licensed under the MIT License. See the `LICENSE` file for more information.
+
+Translated with www.DeepL.com/Translator (free version)
---
+++ app.py
@@ -0,0 +1,140 @@
+import ctypes.wintypes
+import logging
+import os
+import shutil
+import sys
+import time
+
+logging.basicConfig(filename="info.log", level=logging.INFO)
+logging.basicConfig(filename="crash.log", level=logging.ERROR)
+
+CSIDL_PERSONAL = 5  # My Documents
+SHGFP_TYPE_CURRENT = 0  # Get current, not default value
+
+global result, appMainPath, buf, myDocuments, gameList, SII_exe, active_profile, active_save, profileTable, saveTable
+
+buf = ctypes.create_unicode_buffer(ctypes.wintypes.MAX_PATH)
+ctypes.windll.shell32.SHGetFolderPathW(
+    None, CSIDL_PERSONAL, None, SHGFP_TYPE_CURRENT, buf
+)
+myDocumentsPath = buf.value
+print("*****************************************************************")
+print("*        【 Product Name : Scs ETS 2 - Save SII Decrypt 】   *")
+print("*        【 Author: MQuel 】                                    *")
+print("*        【 Github: github@murselsen 】                         *")
+print("*        【 Discord: 35mursel 】                                *")
+print("*        【 Version: v1 】                        b             *")
+print("*        【 License: 2025 - 2030 】                             *")
+print("****************************************************************")
+print("*                                                               *")
+print("*      --gameName: (required)                                   *")
+print("*                                                               *")
+print("*      ets : Euro Truck Simulator 2                             *")
+print("*      ats : American Truck Simulator                           *")
+print("*                                                               *")
+print("*      --profileDisplayName: (required)                         *")
+print("*                                                               *")
+print("*****************************************************************")
+
+
+profileTable = {"ID": [], "Profile": [], "Path": []}
+gameList = {"ets": "Euro Truck Simulator 2", "ats": "American Truck Simulator"}
+SII_exe = os.path.join(os.getcwd(), "profileDecrypt.exe")
+
+
+def exit(status):
+    if status:
+        input("🚪 | Press any key to exit :")
+    else:
+        return True
+
+
+def decrypt(profilesMainDirsPath, profileDisplayName):
+    print("🎮|Profile :", profileDisplayName)
+    _selectProfilePath = os.path.join(os.getcwd(), profileDisplayName)
+    os.chdir(_selectProfilePath)
+    shutil.copyfile(SII_exe, os.getcwd() + "/profileDecrypt.exe")
+    print("🔔 | Status: profileDecrypt.exe has been copied. Please Wait!")
+    logging.info("🔔 | Status: profileDecrypt.exe has been copied. Please Wait!")
+    time.sleep(1.0)
+    cmdDecrypt = "SII.exe profile.sii"
+    print("🔔 | Status: Profile.sii files will be decrypted. Please Wait!")
+    logging.info("🔔 | Status: Profile.sii files will be decrypted. Please Wait!")
+    time.sleep(1.0)
+    os.system(cmdDecrypt)
+    print("🔔 | Status: profileDecrypt.exe has been removed. Please Wait!")
+    logging.info("🔔 | Status: profileDecrypt.exe has been removed. Please Wait!")
+    os.remove("profileDecrypt.exe")
+    os.chdir(profilesMainDirsPath)
+
+
+def main():
+
+    def cwd():
+        label = "📂 | Where I am:  |"
+        labelLength = len(label)
+        cwd = os.getcwd() + " |"
+        cwdLength = len(cwd)
+        print(
+            "\n+",
+            "-".center(labelLength - 1, "-"),
+            "+",
+            "-".center(cwdLength - 2, "-"),
+            "+",
+        )
+        print("|", label, cwd)
+        print(
+            "+",
+            "-".center(labelLength - 1, "-"),
+            "+",
+            "-".center(cwdLength - 2, "-"),
+            "+\n",
+        )
+    # Index: 0
+    # Path: 45545332424F52444F
+    # Profile
+    # Index: 1
+    # Path: 52544853617665
+    # Profile
+    # Index: 2
+    # Path: 666F72756D2E776F746D702E636F6D
+    print(sys.argv)
+    for index, game in enumerate(gameList):
+        print("🎮|App :", gameList.get(game))
+    if sys.argv[1] == "ets" or sys.argv[1] == "ats":
+        _selectGame = sys.argv[1]
+        _selectGameResult = gameList.get(_selectGame)
+        print("\nSelect Game: ", _selectGameResult)
+        _selectGamePath = os.path.join(myDocumentsPath, _selectGameResult, "profiles")
+        os.chdir(_selectGamePath)
+
+        if len(sys.argv) > 2:
+            _selectProfile = sys.argv[2]
+            print("\nSelect Profile: ", _selectProfile)
+            if os.path.isdir(os.path.join(os.getcwd(), _selectProfile)):
+                decrypt(os.getcwd(), _selectProfile)
+            else:
+                logging.warning(
+                    f"⚠️ | Warning: Profile directory '{_selectProfile}' does not exist."
+                )
+        else:
+            print(os.listdir(os.getcwd()))
+            for profileDir_index, profileDir_displayName in enumerate(
+                os.listdir(os.getcwd())
+            ):
+                profileDir_path = os.path.join(os.getcwd(), profileDir_displayName)
+                print("Profile\nIndex:", profileDir_index, "\nPath:", profileDir_path)
+                if os.path.isdir(profileDir_path):
+                    decrypt(os.getcwd(), profileDir_displayName)
+                else:
+                    logging.warning(
+                        f"⚠️ | Warning: Profile directory '{profileDir_displayName}' does not exist."
+                    )
+
+
+try:
+    main()
+    input("🚪 | Press any key to exit :")
+    # exit(1)
+except Exception as e:
+    logging.error("Error: %s", e.args)
